b91c18c533c57598db274813988099238b043cdd
rezaHosseini <rezahosseiy1998@gmail.com>
2025-06-29 17:00:00 +0330

    vercel debbuge

--- server/controllers/roomControllers.js
+++ server/controllers/roomControllers.js
@@ -2,7 +2,7 @@
 
 import Hotel from "../models/Hotel.js";
 import { v2 as cloudinary } from "cloudinary";
-import Room from "../models/room.js";
+import Room from "../models/Room.js";
 
 export const createRoom = async (req, res) => {
   try {
